07db6284449d2ed41974a6e73bd720323b5512fa
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-06-27 21:15:40 -0400

    minor: reorganize shell ref

--- resources/short_shell_ref.sh
+++ resources/short_shell_ref.sh
@@ -1,3 +1,12 @@
+# Copy file
+cp original.txt new.txt
+
+# Make directory
+mkdir mydirectoryname
+
+# Move file; also used for renaming
+mv original.txt new.txt
+
 # Display file
 cat myfile.txt
 
@@ -7,13 +16,4 @@ cat somefile.txt anotherfile.txt
 # Display file with ability to scroll using arrow keys.
 # Press q to quit
 less myfile.txt
-
-# Move file; also used for renaming
-mv original.txt new.txt
-
-# Copy file
-cp original.txt new.txt
-
-# Make directory
-mkdir mydirectoryname
 
