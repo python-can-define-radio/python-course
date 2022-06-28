d7de526ca31411119e09bbe9d5c9b4f2c22caa79
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-06-28 16:11:09 -0400

    Create ex_0_shell.sh

---
+++ classroom_activities/ex_0_shell.sh
@@ -0,0 +1,35 @@
+# 1
+# First, run these:
+cd ~
+cd Desktop
+mkdir mystuff
+cd mystuff
+touch a.txt
+touch b.txt
+
+# 2 
+# After running those commands, use the GUI file browser to look at the files you just created.
+# Open the two text documents and add some content.
+
+
+# ...
+
+# First run these.
+cd /
+cd tmp
+mkdir hooah
+
+# Inside of the hooah directory,
+# make the following folder structure.
+# .
+# └── house
+#     ├── smallstuff
+#     │   ├── pencil
+#     │   ├── marker
+#     │   └── eraser
+#     └── furniture
+#         ├── chair
+#         ├── table
+#         └── desk
+
+
