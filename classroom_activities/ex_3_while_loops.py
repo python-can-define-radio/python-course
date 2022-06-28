967868e42f959424fab30fac5d131d5a74c35457
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-06-28 19:57:14 -0400

    Create ex_3_while_loops.py

---
+++ classroom_activities/ex_3_while_loops.py
@@ -0,0 +1,64 @@
+# 1
+# Try this.
+count = 10
+print(f"count is {count}.")
+count += 1
+print(f"I changed count. Now, it's {count}.")
+count -= 2
+print(f"I changed it again. Now, it's {count}.")
+
+
+# 2
+# Modify the above example so it asks the user for a number.
+
+
+# 3
+# Try this.
+count = 3
+print(count)
+count -= 1
+print(count)
+count -= 1
+print(count)
+count -= 1
+print("Reached zero. Proof:")
+print(count)
+
+
+# 4
+# Let's make the previous example less repetitive.
+# Try this:
+count = 3
+while count > 0:
+    print(count)
+    count -= 1
+print("Reached zero. Proof:")
+print(count)
+
+
+
+# 5
+# Modify the previous example to let the user pick where to start the count.
+
+
+# 6
+# Modify the previous example to display
+# "Launch in 3"
+# "Launch in 2"
+# "Launch in 1"
+# "Liftoff!"
+
+# 7
+# Try this:
+import time
+
+print("Start...")
+time.sleep(1)
+print("Done.")
+
+# 8
+# Modify the previous example to let the user pick the number of delay seconds.
+
+# 9
+# Modify the earlier "Liftoff" example to add a small delay to make it more fun.
+# (It should wait a second after each number is displayed.)
