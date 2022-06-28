aff29105082285a0ee743159afce04cece7e08b2
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-06-27 21:14:54 -0400

    Create short_shell_ref.sh

---
+++ resources/short_shell_ref.sh
@@ -0,0 +1,19 @@
+# Display file
+cat myfile.txt
+
+# Display multiple files, concatenated  (this is the origin of the name cat)
+cat somefile.txt anotherfile.txt
+
+# Display file with ability to scroll using arrow keys.
+# Press q to quit
+less myfile.txt
+
+# Move file; also used for renaming
+mv original.txt new.txt
+
+# Copy file
+cp original.txt new.txt
+
+# Make directory
+mkdir mydirectoryname
+
