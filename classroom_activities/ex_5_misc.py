fab904fb6d234199e330f45819b63367558ea2d4
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-06-28 19:37:03 -0400

    Create ex_5_misc.py

---
+++ classroom_activities/ex_5_misc.py
@@ -0,0 +1,19 @@
+# 1
+# Prompt the user for a string. Print the last three characters using string indexing.
+
+
+# 2
+# Modify the previous question to print “Mike” if the last three characters are MHz, and “Golf” if the last three characters are GHz.
+# If the last three are neither of these, print “No match”.
+
+
+# 3
+# Prompt the user for a string. Print all of the characters EXCEPT the last three characters.
+
+
+# 4
+# Prompt the user for a number with MHz. (example input: 2463 MHz.) Print the numerical part. You may assume the user formats the input correctly, but you cannot assume the length of the number.
+
+
+# 5
+# Modify the previous question so that it converts the given number from MHz to GHz.
