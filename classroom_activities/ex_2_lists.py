738fa367d28dac625a7c944421604dcca4e567e6
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-06-29 14:48:32 -0400

    Update ex_2_lists.py

--- classroom_activities/ex_2_lists.py
+++ classroom_activities/ex_2_lists.py
@@ -1,43 +1,77 @@
-    Try this. Do you expect it to print “water” or “chair”?[Text Wrapping Break]thelist = ["water", "chair", "mug", "mouse"][Text Wrapping Break]print(thelist[1])[Text Wrapping Break] 
+# 1
+# Try this. Do you expect it to print “water” or “chair”?
+thelist = ["water", "chair", "mug", "mouse"]
+print(thelist[1])
 
-    Modify the previous example to print the fourth element of the list (“mouse”). [Text Wrapping Break] 
+# 2
+# Modify the previous example to print the fourth element of the list (“mouse”).
 
-    Try this. Do you expect an error?[Text Wrapping Break]print(thelist[-1])[Text Wrapping Break] 
+# 3
+# Try this. Do you expect an error?
+print(thelist[-1])
 
-    Add a few elements to thelist. Then do this again:[Text Wrapping Break]print(thelist[-1]) 
+# 4
+# Add a few elements to thelist. Then do this again:
+print(thelist[-1])
 
- 
 
-    Now make it print the second-to-last element of thelist.[Text Wrapping Break] 
+# 5
+# Now make it print the second-to-last element of thelist.
 
-    Try this. It’s a little counterintuitive.[Text Wrapping Break]print(thelist[0:3])[Text Wrapping Break] 
 
-    Try this.[Text Wrapping Break]print(thelist[1:3]) 
+# 6
+# Try this. It’s a little counterintuitive.
+print(thelist[0:3])
 
- 
 
-    Modify the previous example to print elements 0,1,2,3. (Hint: it’s not going to be [0:3], but it’s close to that.)[Text Wrapping Break] 
+# 7
+# Try this.
+print(thelist[1:3])
 
-    Try this.[Text Wrapping Break]print(thelist[1:])[Text Wrapping Break] 
 
-    Modify the previous example to print all but the first two items. Pretend you don’t know how long the list is. (You shouldn’t need to.)[Text Wrapping Break] 
+# 8
+# Modify the previous example to print elements 0,1,2,3. (Hint: it’s not going to be [0:3], but it’s close to that.)
 
-    Try this.[Text Wrapping Break]letters = [“a”, “b”, “c”, “d”, “e”][Text Wrapping Break]letters[0] = “ROCKET”[Text Wrapping Break] 
+# 9
+# Try this.
+print(thelist[1:])
 
-    Modify the previous example to replace “b” with “ROCKET” instead of replacing “a”.[Text Wrapping Break] 
+# 10
+# Modify the previous example to print all but the first two items.
+# Pretend you don’t know how long the list is. (You shouldn’t need to.)
 
-    Modify the previous example to replace the second-to-last element with “ROCKET”.[Text Wrapping Break] 
 
-    Try this.[Text Wrapping Break]name = “Becky”[Text Wrapping Break]print(name[0])[Text Wrapping Break] 
+# 11
+# Try this.
+letters = ["a", "b", "c", "d", "e"]
+letters[0] = "ROCKET"
 
-    Modify the previous example to print the “c” of “Becky”.[Text Wrapping Break] 
 
- 
+# 12
+# Modify the previous example to replace “b” with “ROCKET” instead of replacing “a”.
 
- 
 
-    Try this.[Text Wrapping Break]names = [“Sarah”, “Tom”, “Harry”, “Susan”, “Becky”][Text Wrapping Break]print(names[4][3])[Text Wrapping Break] 
+# 13
+# Modify the previous example to replace the second-to-last element with “ROCKET”.
 
-    Modify the previous example to print the last letter of Sarah’s name (using string indexing).[Text Wrapping Break] 
 
-    Modify the previous example to print the last letter of the last name in the list. Use negative indexing.[Text Wrapping Break] 
+# 14
+# Try this.
+name = "Becky"
+print(name[0])
+
+# 15
+# Modify the previous example to print the “c” of “Becky”.
+
+
+# 16
+# Try this.
+names = ["Sarah", "Tom", "Harry", "Susan", "Becky"]
+print(names[4][3])
+
+# 17
+# Modify the previous example to print the last letter of Sarah’s name (using string indexing).
+
+# 18
+# Modify the previous example to print the last letter of the last name in the list. Use negative indexing.
+
