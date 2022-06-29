37cc930f00090b3fb402576bffb8432116bb45f5
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-06-29 14:51:36 -0400

    Update ex_4_files.py

--- classroom_activities/ex_4_files.py
+++ classroom_activities/ex_4_files.py
@@ -1,6 +1,19 @@
 # 1
-# Try this.
+# Try this. 
+# It won't display anything to the screen -- rather, it creates a file.
+# Try to find the file it created.
 
 f = open("something_very_unique_file.txt", "w")
 f.write("Here are some words.")
 f.close()
+
+
+
+# 2
+# Try this.
+f = open("something_very_unique_file.txt", "r")
+contents = f.read()
+print(contents)
+f.close()
+
+
