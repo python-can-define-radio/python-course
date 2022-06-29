496c0104b7c1d32c68a34a46ae936e9836a31c58
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-06-29 15:15:47 -0400

    Create ex_randomness.py

---
+++ classroom_activities/ex_randomness.py
@@ -0,0 +1,10 @@
+# Before starting the exercise, type this at the top of your file:
+import random
+
+
+# Once you've added that, try these exercises.
+
+# 1
+# Try this.
+things = ["dinosaur", "dog", "cat"]
+print(random.choice(things))
