caeacb32b15d7a1f469ab6080e9df99c53e4bf83
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-06-30 16:21:35 -0400

    More exercises

--- classroom_activities/ex_1_print.py
+++ classroom_activities/ex_1_print.py
@@ -1,2 +1,148 @@
-# Try this.
+## 1 
+## Try this.
 print("Here we go!")
+
+
+## 2
+## Try this.
+## You'll get an error having to do with the quotation marks.
+## Why do you think that is? How do you fix it?
+print(“bob”)
+
+
+## 3
+## Try this.
+firstn = "Bob"
+lastn = "Smith"
+print(firstn)
+print(lastn)
+
+
+## 4
+## Try this.
+firstn = "Bob"
+lastn = "Smith"
+print(f"My name is {firstn} {lastn}")
+
+
+## 5
+## Try this.
+firstn = input("What is your first name?")
+lastn = "Smith"
+print(f"Your name is {firstn} {lastn}.")
+
+
+## 6
+## Copy and modify the previous example so that it asks the user for his/her last name,
+## instead of assuming the last name is Smith.
+
+
+## 7
+## Ask the user for a color, and then say
+## "___ is a pretty color."
+## (Fill in the blank with the actual color.)
+
+
+## 8
+## Ask the user for the name of an animal and a plant.
+## Then display "The ___ eats ___ every day",
+## but fill in the blanks with the animal and the plant.
+
+
+## 9
+## Try this.
+a = "Hello"
+b = "Goodbye"
+c = a + b
+print(c)
+
+
+## 10
+## Try this.
+a = input("First word? ")
+b = input("Second word? ")
+c = a + b
+print(c)
+
+
+## 11
+## Try this.
+a = 5
+b = 3
+c = a + b
+print(c)
+
+
+## 12
+## Try this.
+a = "5"
+b = "3"
+c = a + b
+print(c)
+
+
+## 13 
+## Try this.
+a = int("5")
+b = int("3")
+c = a + b
+print(c)
+
+
+## 14
+## Try this.
+a = input("Type an integer.")
+b = input("Type another integer.")
+c = a + b
+print(c)
+
+
+## 15
+## Try this.
+a = int(input("Type an integer."))
+b = int(input("Type another integer."))
+c = a + b
+print(c)
+
+
+## 16
+## Try this. You will get an error. How do you fix it?
+a = input("Type an integer.")
+b = int(input("Type another integer."))
+c = a + b
+print(c)
+
+## 17
+## Try this. You will get an error. How do you fix it?
+favnum = input("What is your favorite number?")
+onemore = favnum + 1
+print(f"One more would be {onemore}.")
+
+
+## 18
+## Ask the user for two integers.
+## Display "The sum of your two numbers is ___".
+
+
+## 19
+## Ask the user for two integers.
+## Display "The first number minus the second number is ___".
+
+
+## 20
+## Ask the user for an integer.
+## Display "That number plus 2 is ___".
+
+
+## 21
+## Ask the user for a number.
+## Display "Half of that number is ___".
+
+
+## 22
+## Ask the user for a number of questions on a test,
+## and ask "how many did you get right?"
+## Then, display "You got ___% right". (You'll need to calculate the percent.)
+
+
+
