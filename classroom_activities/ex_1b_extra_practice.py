19cc0bd18d522dfbc549d2a4bd083df7e3f4213f
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-07-05 11:18:14 -0400

    Create ex_1b_extra_practice.py

---
+++ classroom_activities/ex_1b_extra_practice.py
@@ -0,0 +1,10 @@
+## 1
+## Try this.
+thename = input("What is your name? ")
+if thename == "George":
+    print("Are you named after the first US President?")
+else:
+    print("Hello.")
+
+## 2
+## Change the previous example so that if the user types "Bob", it will reply "Are you the painter?"
