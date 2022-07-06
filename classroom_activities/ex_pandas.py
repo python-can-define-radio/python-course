49e4ab1a583806bab3851861718c658b1abf8023
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-07-06 15:14:04 -0400

    Update ex_pandas.py

--- classroom_activities/ex_pandas.py
+++ classroom_activities/ex_pandas.py
@@ -3,30 +3,38 @@
 ## - Download the fake smoker data csv file from our classroom_activities folder.
 ##   (Sidenote for those who are interested: the code I used to generate the fake smoker data is at the bottom of this file.)
 
-import pandas as pd
-
-## Setup
-fakeSmokerData = pd.read_csv("fake_smoker_data.csv")
 
 ## 1
-print(fakeSmokerData)
+## Verifying that the data is available:
+f = open("fake_smoker_data.csv")
+print(f.readlines())
+f.close()
+
 
 ## 2
+## Setup
+import pandas as pd
+fakeSmokerData = pd.read_csv("fake_smoker_data.csv")
+
+## 3
+## Try this:
+print(fakeSmokerData)
+
+## 4
 ## This is just the index of those who started smoking before age 20.
 print(fakeSmokerData["age_started_smoking"] < 20)
 
-## 3
+## 5
 ## If we want to see the rest of the data for those rows, we do this.
 print(fakeSmokerData[fakeSmokerData["age_started_smoking"] < 20])
 
-## 4
+## 6
 ## We can see how many people strted smoking before 20:
 below20 = fakeSmokerData[fakeSmokerData["age_started_smoking"] < 20]
 print(len(below20))
 
-## 5
+## 7
 ## Print the rows of people who are currently older than 50.
-
 
 
 
