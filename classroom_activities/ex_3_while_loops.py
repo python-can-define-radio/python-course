d528cd2eb818040e18f3507361520cdf216cabe0
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-07-06 23:53:42 -0400

    more while loops examples, and add cond mistake

--- classroom_activities/ex_3_while_loops.py
+++ classroom_activities/ex_3_while_loops.py
@@ -56,10 +56,93 @@ print("Start...")
 time.sleep(1)
 print("Done.")
 
+
 ## 8
 ## Modify the previous example to let the user pick the number of delay seconds.
+
 
 ## 9
 ## Modify the earlier "Liftoff" example to add a small delay to make it more fun.
 ## (It should wait a second after each number is displayed.)
+
+
+## 10
+## Try this.
+
+keepGoing = "yes"
+while keepGoing == "yes":
+    print("Since the variable keepGoing is still 'yes', I am going to keep going.")
+    keepGoing = input("Do you want me to keep going? ")
+print("Ok, you typed something other than 'yes', so I stopped.")
+
+
+## 11
+## Copy and modify the previous example like so:
+## If the user types "y", then keep looping.
+
+
+## 12
+## Try this:
+keepGoing = "yes"
+while keepGoing == "yes" or keepGoing == "y":
+    print("Continuing.")
+    keepGoing = input("Do you want me to keep going? ")
+print("Ok, stopping.")
+
+
+## 13
+## Copy and modify the previous example like so:
+## Continue looping if the user types any of these: "hey", "woo", or "yes".
+## Note: I recommend trying the two exercises in the file ex_common_condition_mistake.py
+##       to avoid a common subtle mistake.
+
+
+## 14
+## Copy and modify the previous example like so:
+## If the user types anything other than "no", then keep going.
+
+
+## 15
+## Try this.
+
+print("Welcome to the word guesser!")
+guess = ""
+secretWord = "water"
+while guess != secretWord:
+    guess = input("What is your guess? ")
+print("You got it!")
+
+
+## 16
+## Copy and modify the previous example so that each iteration
+## of the loop displays "Let me check if that's right."
+
+
+## 17
+## Copy and modify the previous example so that each 
+## iteration of the loop displays "No, try again"
+## (but only if the guess was wrong).
+
+
+## 18
+## Try this.
+## Note: we must set `num` to something before the loop starts,
+##       so I arbitrarily picked 0.
+
+
+print("Welcome to the number doubler.")
+num = 0
+while num != -1:
+    num = int(input("Type a number, or type -1 to quit: "))
+    print(f"Double your num is {num * 2}.")
+
+
+## 19
+## Copy and modify the previous example so that instead of doubling numbers,
+## it calculates the number squared.
+
+
+## 20
+## Copy and modify the previous example so that it does not display
+## the doubled number if the user enters -1.
 
---
+++ classroom_activities/ex_common_condition_mistake.py
@@ -0,0 +1,21 @@
+## 1
+## Try this.
+guess = input("Guess one of my words: ")
+if guess == "water" or guess == "food":
+    print("You were right.")
+else:
+    print("Sorry, not right.")
+
+
+## 2
+## Try this. Test it thoroughly.
+## (DON'T DO THIS IN REAL CODE. You'll see why.)
+guess = input("Guess one of my words: ")
+if guess == "water" or "food":
+    print("You were right.")
+else:
+    print("Sorry, not right.")
+
+## 3
+## Ask an instructor if you are unclear about the purpose
+## of those two exercises.
