469817a44e3d949fc6daaaf26298c027ebad865d
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-07-07 19:37:43 -0400

    Update other_cool_examples.py

--- classroom_activities/other_cool_examples.py
+++ classroom_activities/other_cool_examples.py
@@ -15,7 +15,20 @@ for oldname in thefiles:
     if oldname.endswith(".fake"):
 
         # Make a new name
+        # (See also note below)
         newname = oldname.replace(".fake", ".awesome")
 
         # Actually do the rename
         os.rename(oldname, newname)
+
+## Note:
+## If the goal is to just change the file extension, then this
+## may do more than desired -- if the file were, for example,
+## my.fake.file.with.a.long.name.fake
+## then both occurences of ".fake" would be replaced,
+## rather than just the extension.
+## The better (but somewhat more ugly) way would be to do something like this:
+## extension = ".fake"
+## if oldname.endswith(extension)
+##    firstPart = oldname[:-len(extension)]
+##    newname = firstPart + ".awesome"
