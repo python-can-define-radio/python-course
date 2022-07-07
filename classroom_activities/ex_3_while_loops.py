1c419cdaf6447a8bb811b67d2ad2393b9fd899d9
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-07-06 23:17:44 -0400

    "##" instead of "#"

--- classroom_activities/ex_3_while_loops.py
+++ classroom_activities/ex_3_while_loops.py
@@ -1,5 +1,5 @@
-# 1
-# Try this.
+## 1
+## Try this.
 count = 10
 print(f"count is {count}.")
 count += 1
@@ -8,12 +8,12 @@ count -= 2
 print(f"I changed it again. Now, it's {count}.")
 
 
-# 2
-# Modify the above example so it asks the user for a number.
+## 2
+## Modify the above example so it asks the user for a number.
 
 
-# 3
-# Try this.
+## 3
+## Try this.
 count = 3
 print(count)
 count -= 1
@@ -25,9 +25,9 @@ print("Reached zero. Proof:")
 print(count)
 
 
-# 4
-# Let's make the previous example less repetitive.
-# Try this:
+## 4
+## Let's make the previous example less repetitive.
+## Try this:
 count = 3
 while count > 0:
     print(count)
@@ -37,28 +37,29 @@ print(count)
 
 
 
-# 5
-# Modify the previous example to let the user pick where to start the count.
+## 5
+## Modify the previous example to let the user pick where to start the count.
 
 
-# 6
-# Modify the previous example to display
-# "Launch in 3"
-# "Launch in 2"
-# "Launch in 1"
-# "Liftoff!"
+## 6
+## Modify the previous example to display
+## "Launch in 3"
+## "Launch in 2"
+## "Launch in 1"
+## "Liftoff!"
 
-# 7
-# Try this:
+## 7
+## Try this:
 import time
 
 print("Start...")
 time.sleep(1)
 print("Done.")
 
-# 8
-# Modify the previous example to let the user pick the number of delay seconds.
+## 8
+## Modify the previous example to let the user pick the number of delay seconds.
 
-# 9
-# Modify the earlier "Liftoff" example to add a small delay to make it more fun.
-# (It should wait a second after each number is displayed.)
+## 9
+## Modify the earlier "Liftoff" example to add a small delay to make it more fun.
+## (It should wait a second after each number is displayed.)
+
