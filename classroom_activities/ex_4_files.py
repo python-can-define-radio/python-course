99aec2b53b14bac52df54947ad20927acf69f3be
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-07-11 09:33:18 -0400

    Update ex_4_files.py

--- classroom_activities/ex_4_files.py
+++ classroom_activities/ex_4_files.py
@@ -4,16 +4,32 @@
 # Try to find the file it created.
 
 f = open("something_very_unique_file.txt", "w")
-f.write("Here are some words.")
+f.write("Here are some words.\n")
+f.write("More words.")
 f.close()
 
 
-
 # 2
+# Using Python,
+# Open the file my_thoughts.txt.
+# Write at least 10 separate lines in it.
+# Close the file.
+
+
+# 3
+# Using Python,
+# Ask the user for name and age.
+# Open a file called person_info.txt.
+# Write in that file, "___ is ___ years old" (with the name and age filled in).
+# Close the file.
+
+
+# 4
 # Try this.
 f = open("something_very_unique_file.txt", "r")
 contents = f.read()
 print(contents)
 f.close()
+
 
 
