87d2fbbe4282ab1a67390603470f76ce37ee1e1f
m-pcdr <109978762+m-pcdr@users.noreply.github.com>
2022-07-25 13:10:38 -0400

    Update ex_7b_csv_files.py

--- classroom_activities/ex_7b_csv_files.py
+++ classroom_activities/ex_7b_csv_files.py
@@ -1,2 +1,7 @@
- a
+"ex_7b_csv_files.py" --- Writing & Reading Spreadsheet Files, specifically CSV files.
+> In this lesson will we will use the Python programming language to work with "comma-separated-value" or "CSV" files, which are only one of several types of spreadsheet files.
+> Background: CSV files may include multiple data records with each data record located on its own line.  Each data record (i.e., each line in the file) may include multiple pieces of data separated by commas.  On each line, the pieces of data are always stored in the same sequence.  When saved, the filee name should end with the file extension ".csv"
+>In this lesson, we will start by making Python create or write a CSV file.  Alternatively, a CSV file may be manually created using a text editor or MS Visual Studio Code, as examples.  Subsequently, we will create a simple Python program to read data from the CSV file.
   
+ #1 Goal:  Write data to a CSV file.  This will create a new file or will over-write an existing file.
+ 
