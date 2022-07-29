9edf0c180e2f3a7244a0c43a660aa2816d9949ab
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-07-29 16:05:53 -0400

    Update ex_3_if_else.py

--- classroom_activities/ex_3_if_else.py
+++ classroom_activities/ex_3_if_else.py
@@ -192,8 +192,28 @@ print("C")
 ## 22
 ## Write a program that takes a number from the user. Display the number doubled. Then do a sequence of creative if statements of your choice. Some examples: if the number is greater than 30, display "that’s pretty big." If the number is negative, display "Really? Negative? Interesting".  
 
- 
 ## 23
+## Try this.
+x = float(input("Type a number between 0 and 1, for example, 0.3, 0.25, etc... "))
+print("One more would be {x + 1}.")
+
+## As you can see, you can use `float` instead of `int` when dealing with non-whole numbers.
+## NOTE: Using floats can cause weird rounding errors. For example:
+print(1.03 - .42)
+## This will print 0.6100000000000001.
+## That's quite important when doing comparisons:
+if .1 + .1 + .1 == .3:
+    print("This will not print.")
+else:
+    print("This will print... what is math??")
+    
+## The reason is because the numbers are converted to binary behind the scenes.
+
+## For more info, see https://docs.python.org/3/tutorial/floatingpoint.html
+## If you plan to eventually do any "real-life" programming, then it's definitely worth reading.
+
+
+## 23b
 ## Ask the user for the cost of a single item and the quantity purchased. Print the total cost. 
 ## Make sure this works for non-integer costs. For example, try a cost of 2.30 and quantity of 2.
 ## Hint: You'll use float instead of int.
