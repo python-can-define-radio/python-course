aba16b018f7d478081e494351fa88dd0eab567fd
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-08-05 13:47:58 -0400

    Create ex_2b_operators.py

---
+++ classroom_activities/ex_2b_operators.py
@@ -0,0 +1,4 @@
+## 1
+## Ask the user for two numbers.
+## Display "If you divided ___ by ___, then you would get ____ with a remainder of ___".
+## You'll want the modulus operator. Look up how to do remainder (or modulus) in Python.
