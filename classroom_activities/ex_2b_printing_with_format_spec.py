77ff27f766afb0c8afd130a09c47448ec671c646
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-08-31 15:34:36 -0400

    Create ex_2b_printing_with_format_spec.py

---
+++ classroom_activities/ex_2b_printing_with_format_spec.py
@@ -0,0 +1,20 @@
+## Sometimes, you want to print with some specific format.
+## For example:
+##  - Rounding a number to two decimal places
+##  - Padding a number with spaces so that it aligns with other numbers in a list
+
+## For example:
+item_number = 27.283
+print(f"My item num is {item_number}.")
+print(f"My item num is {item_number:10} and I put some space with it.")
+print(f"Rounded would be {item_number:.2f} with two decimal places.")
+
+## This website gives some great practical examples. Recommended to start here.
+## https://www.linisnil.com/articles/practical-guide-to-python-string-format-specifiers/
+
+## This website gives detailed documentation.
+## https://docs.python.org/3/library/string.html#format-specification-mini-language
+
+
+
+
