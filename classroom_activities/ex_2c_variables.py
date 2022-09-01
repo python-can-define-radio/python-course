8d2b4cf0ebc0abfa00893742a325039353278bfe
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-09-01 15:56:26 -0400

    Create ex_2c_variables.py

---
+++ classroom_activities/ex_2c_variables.py
@@ -0,0 +1,25 @@
+## These are valid variable names:
+mynumber = 7
+temp_in_f = 8.3
+my2ndthing = "hi"
+x3 = "stuff"
+_myStuff = 7
+
+# This is valid, but please don't, because input will no longer be a function
+input = 7
+
+#############
+# invalid:
+#############
+
+# Can't use keywords
+if = 6
+
+# Can't start with number
+3rdthing = "hi"
+
+# Can't have spaces inside of the name
+my var = 3
+
+# Can't have special characters (only "_" is allowed inside variable name)
+$d = 6
