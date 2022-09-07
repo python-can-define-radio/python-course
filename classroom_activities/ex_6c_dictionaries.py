acc605a148ebe34d60f0ab80f5b768c37484fd4e
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-09-07 15:51:39 -0400

    Create ex_6c_dictionaries.py

---
+++ classroom_activities/ex_6c_dictionaries.py
@@ -0,0 +1,35 @@
+## 1
+## Try this.
+print("If you want to put everything", end="")
+print("on one line, then you can", end="")
+print("use this.")
+
+
+## 2
+## Try this.
+mypersondata = {
+  "firstname": "Bob",
+  "lastname": "Smith,
+  "age": 23
+}
+print(mypersondata["firstname"])
+
+
+## 3
+## Copy and modify the previous example so that it displays the last name instead of the first name.
+
+
+## 4
+## Try this.
+mypersondata = {
+  "firstname": "Bob",
+  "lastname": "Smith,
+  "age": 23
+}
+print(f"Do you know {mypersondata["firstname"]} ...?")
+
+
+## 5
+## Copy and modify the previous example so that it displays this:
+##   "Did you know that ___   ___ is ___ years old?"
+##   (Fill in the first name, last name, and age appropriately.)
