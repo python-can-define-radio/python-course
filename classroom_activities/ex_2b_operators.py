38d12399d2b562bb47787fb1876716ef3faef7ac
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-09-07 08:13:49 -0400

    ops: add exponent exercise

--- classroom_activities/ex_2b_operators.py
+++ classroom_activities/ex_2b_operators.py
@@ -2,3 +2,9 @@
 ## Ask the user for two numbers.
 ## Display "If you divided ___ by ___, then you would get ____ with a remainder of ___".
 ## You'll want the modulus operator. Look up how to do remainder (or modulus) in Python.
+
+
+## 2
+## Ask the user for two numbers.
+## Display "If you computed ___ to the power of ___, then you would get ___."
+## (Hint: What is the operator for exponent in Python? It may not be what you think.)
