a17339ccc3fa756f93e5aad629a2be08e54f4a03
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2022-09-12 08:22:28 -0400

    Update ex_8b_turtle_pong.py

--- classroom_activities/ex_8b_turtle_pong.py
+++ classroom_activities/ex_8b_turtle_pong.py
@@ -1,25 +1,51 @@
 import turtle
 import time
 import random
+import sys
+
+
+TOP = 400
+BOTTOM = -400
+LEFT_PADDLE = -500
+LEFT_WALL = -550
+RIGHT_PADDLE = 500
+RIGHT_WALL = 550
+
+wall_drawer = turtle.Turtle()
+wall_drawer.hideturtle()
+time.sleep(1)
+
+# Turtles always start at (0, 0)
+# So we must pick up the pen and go to the corner
+wall_drawer.penup()
+wall_drawer.goto(LEFT_WALL, TOP)
+wall_drawer.pendown()
+wall_drawer.pensize(12)
+
+wall_drawer.goto(RIGHT_WALL, TOP)
+wall_drawer.goto(RIGHT_WALL, BOTTOM)
+wall_drawer.goto(LEFT_WALL, BOTTOM)
+wall_drawer.goto(LEFT_WALL, TOP)
+
+
+ball = turtle.Turtle()
+ball.penup()
+ball.shape("circle")
 
 paddle1 = turtle.Turtle()
 paddle1.color("purple")
-time.sleep(3)
-
 paddle1.penup()
 paddle1.shape("square")
 paddle1.shapesize(5, 1)  # stretch so it looks like a paddle
-paddle1.goto(-300, 0)
+paddle1.goto(LEFT_PADDLE, 0)
 
 paddle2 = turtle.Turtle()
 paddle2.penup()
 paddle2.shape("square")
 paddle2.shapesize(5, 1)
-paddle2.goto(300, 0)
+paddle2.goto(RIGHT_PADDLE, 0)
 
-ball = turtle.Turtle()
-ball.penup()
-ball.shape("circle")
+
 
 
 
@@ -42,8 +68,22 @@ def p2_down():
     x, y = paddle2.pos()
     paddle2.sety(y - 10)
 
+
+def ball_touching_wall():
+    bx, by = ball.pos()
+    if by >= TOP:
+        time.sleep(5)
+    if by <= BOTTOM:
+        time.sleep(5)
+
+
+def ricochet():
+    print("TODO")
+
 def handle_bounce():
-    print("IN PROGRESS")
+    if ball_touching_wall():
+        ricochet()
+    print("TODO")
     # if ball.distance(paddle1)
 
     # ball.distance(paddle2)
@@ -58,7 +98,8 @@ turtle.onkeypress(p2_down, "Down")
 
 turtle.listen()
 
-init_direction = random.randint(0, 359)
+# init_direction = random.randint(0, 359)
+init_direction = 270
 ball.setheading(init_direction)
 
 # loop forever
