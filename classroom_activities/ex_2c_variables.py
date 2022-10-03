c9eff4ddf059ea349f6c6409e16c77fb6cdf9564
m-pcdr <109978762+m-pcdr@users.noreply.github.com>
2022-10-03 14:36:41 -0400

    Update ex_2c_variables.py

--- classroom_activities/ex_2c_variables.py
+++ classroom_activities/ex_2c_variables.py
@@ -1,3 +1,13 @@
+""" Example 2c Variables -- Python Practice 
+    webpage for this lesson: "https://github.com/python-can-define-radio/python-course/blob/main/classroom_activities/ex_2b_printing_with_format_spec.py"https://github.com/python-can-define-radio/python-course/blob/main/classroom_activities/ex_2c_variables.py"
+
+In programming languagues, variables are allocations of computer memory assigned to store data specified by the programmer. 
+
+Fdiable naming rules/conventions for Python:
+  https://pythonguides.com/python-naming-conventions/
+  
+""" 
+
 ## These are valid variable names:
 mynumber = 7
 temp_in_f = 8.3
