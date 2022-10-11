e5dadc7c5dfc4d548c1dbb374a7c673f2afeb087
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2022-10-11 08:54:38 -0400

    cost qty: add example

--- classroom_activities/ex_3_if_else.py
+++ classroom_activities/ex_3_if_else.py
@@ -310,6 +310,10 @@ else:
 ## Ask the user for the cost of a single item and the quantity purchased. Print the total cost. 
 ## Make sure this works for non-integer costs. For example, try a cost of 2.30 and quantity of 2.
 ## Hint: You'll use float instead of int.
+## Example:
+## What is the cost for an item? 2.30
+## How many did you buy? 2
+## The total cost would be $4.60.
  
 ## 24
 ## Modify the previous example so that the shop gives a discount of 10% if you buy at least 20 of an item.  
