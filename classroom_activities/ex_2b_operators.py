b1f9dd380bc479b89bb2766cb83a7c2ac137bfaf
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2022-10-13 14:32:52 -0400

    floor div

--- classroom_activities/ex_2b_operators.py
+++ classroom_activities/ex_2b_operators.py
@@ -2,6 +2,7 @@
 ## Ask the user for two numbers.
 ## Display "If you divided ___ by ___, then you would get ____ with a remainder of ___".
 ## You'll want the modulus operator. Look up how to do remainder (or modulus) in Python.
+## You may also want the floor division operator.
 
 
 ## 2 Calculating an exponent
