e89c192bb87c1b09c7330f23ff2052dbdd343e08
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2022-10-14 11:31:38 -0400

    add copy/paste

--- resources/short_shell_ref.sh
+++ resources/short_shell_ref.sh
@@ -32,3 +32,70 @@ tree
 # Press q to quit
 less myfile.txt
 
+# ------------------------------------
+# below is a semi-overlapping list that I plan to de-dupe
+
+Terminal commands
+- cd: change directory
+
+   Example:
+    ~/Desktop/mystuff$ cd food
+    ~/Desktop/mystuff/food$
+
+   Example:
+    ~/Desktop/mystuff/food$ cd ..
+    ~/Desktop/mystuff$
+
+   
+- mkdir: make directory (folder)
+
+- help: provide help on a command
+
+   Example:
+    $ help cd
+
+- info: provide info on a command 
+  (useful when help doesn't work)
+  Press q to quit when you're done.
+
+   Example:
+    $ info cd
+
+- tree
+
+- echo: Can be used for displaying to the screen, OR for writing to files.
+
+   Example (display to screen):
+    $ echo "hello world"
+
+   Example (write to file):
+    $ echo "hello world" > myfile.txt
+
+   Example (append to file):
+    $ echo "hello world" >> myfile.txt
+
+
+- ls: List directory contents
+
+- touch: create an empty file
+
+- mv: move or rename
+
+   Example (relocate):
+    $ touch a.txt
+    $ mkdir stuff
+    $ ls
+    a.txt  stuff
+    $ mv a.txt stuff
+    $ ls
+    stuff
+
+
+   Example (rename):
+    $ ls
+    thing.txt  other.txt
+    $ mv thing.txt newname.txt
+    $ ls
+    newname.txt  other.txt
+
+- cat: Display file contents
