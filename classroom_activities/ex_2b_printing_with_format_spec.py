9425962d65beaefe74a4e6515c49785b81cbf768
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2023-01-30 10:30:38 -0500

    add more format spec examples

--- classroom_activities/ex_2b_printing_with_format_spec.py
+++ classroom_activities/ex_2b_printing_with_format_spec.py
@@ -12,6 +12,9 @@ item_number = 27.283
 print(f"My item num is {item_number}.")
 print(f"My item num is {item_number:10} and I put some space with it.")
 print(f"Rounded would be {item_number:.2f} with two decimal places.")
+print(f"Rounded AND with some space would be {item_number:9.2f}; do you see?")
+print(f"If the extra space is small, it may not change how the number is printed: {item_number:3.2f} as you see.")
+
 
 ## This website gives some great practical examples. Recommended to start here.
 ## https://www.linisnil.com/articles/practical-guide-to-python-string-format-specifiers/
