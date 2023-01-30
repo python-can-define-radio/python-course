517a30e41a372ed655e93a17105ef58ba2d2fd99
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2023-01-30 16:13:21 -0500

    x = x + 5, and the += op

--- classroom_activities/ex_2b_operators.py
+++ classroom_activities/ex_2b_operators.py
@@ -14,3 +14,34 @@
 ## 3: Operators that may not do what you expect
 ## Based on question #1, does % calculate a percentage? (Answer: No)
 ## Based on question #2, does ^ calculate an exponent? (Answer: No)
+
+
+## 4
+## Try this. As you can see, it's possible to reassign variables.
+x = 3
+print("Currently, x is...")
+print(x)
+x = 4
+print("Now, x is"...)
+print(x)
+
+
+## 5
+## Try this. It's a little counter-intuitive.
+x = 3
+print("Currently, x is...")
+print(x)
+## You should read the line below as "Set x to 5 more than what x used to be."
+x = x + 5
+print("Now, x is"...)
+print(x)
+
+
+## 6
+## Python provides another equivalent way to do this:
+x = 3
+print("Currently, x is...")
+print(x)
+x += 5
+print("Now, x is"...)
+print(x)
