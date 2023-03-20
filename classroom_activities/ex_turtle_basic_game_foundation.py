3c06df11eb4956c1eb51f5304c45f70b72c14594
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2023-03-20 11:55:10 -0400

    make list of blocks

--- classroom_activities/ex_turtle_basic_game_foundation.py
+++ classroom_activities/ex_turtle_basic_game_foundation.py
@@ -1,89 +1,89 @@
 """Arrow keys to move."""
 
 import turtle
-
-
+import random
+ 
+ 
+ 
 WIDTH = 500
 HEIGHT = 600
 turtle.setup(WIDTH + 50, HEIGHT + 50)
 turtle.screensize(100, 3000, "lightblue")
-
+ 
 playerState = {
     "health": 3
 }
-
-
+ 
+ 
 def touching(t1, t2):
+    # type: (turtle.Turtle, turtle.Turtle) -> bool
     return t1.distance(t2) < 5
-
-
+ 
+ 
 def checkCollision():
-    if touching(player, block) or touching(player, block2):
-        
-        info.undo()  # erase the old "health" text on the screen
-        info.write(f"Health: {playerState['health']}")
-
-        playerState["health"] = playerState["health"] - 1
-        player.undo()  # Make the player back up
-
-
+    for block in blocks:
+        if touching(player, block):
+            info.undo()  # erase the old "health" text on the screen
+            info.write(f"Health: {playerState['health']}")
+ 
+            playerState["health"] = playerState["health"] - 1
+            player.undo()  # Make the player back up
+ 
+ 
 def mvright():
     x, y = player.pos()
     if x < WIDTH / 2:
         player.setx(x + 20)
     checkCollision()
-
-
+ 
+ 
 def mvleft():
     x, y = player.pos()
     if x > -WIDTH / 2:
         player.setx(x - 20)
     checkCollision()
-
-
+ 
+ 
 def mvup():
     x, y = player.pos()
     if y < HEIGHT / 2:
         player.sety(y + 20)
     checkCollision()
-
-
+ 
+ 
 def mvdown():
     x, y = player.pos()
     if y > -WIDTH / 2:
         player.sety(y - 20)
     checkCollision()
-            
-
-
-block = turtle.Turtle()
-block.shape("square")
-block.color("black", "brown")
-block.penup()
-block.goto(-60, 20)
-
-block2 = turtle.Turtle()
-block2.shape("square")
-block2.color("black", "brown")
-block2.penup()
-block2.goto(80, 0)
-
+ 
+ 
+blocks = []
+for count in range(30):
+    block = turtle.Turtle()
+    block.shape("square")
+    block.color("black", "brown")
+    block.penup()
+    x = random.randrange(-160, 160+20, 20)
+    y = random.randrange(-160, 160+20, 20)
+    block.goto(x, y)
+    blocks.append(block)
+ 
+ 
 player = turtle.Turtle()
 player.speed(0)
 player.penup()
 player.shape("square")
-
+ 
 info = turtle.Turtle()
 info.penup()
-info.goto(0, 200)
+info.goto(0, 300)
 info.write(f"Health: {playerState['health']}")
-
+ 
 turtle.onkeypress(mvright, "Right")
 turtle.onkeypress(mvleft, "Left")
 turtle.onkeypress(mvup, "Up")
 turtle.onkeypress(mvdown, "Down")
 turtle.listen()
-
+ 
 turtle.mainloop()
-
-
