537f59c139fd217df1d5ed9b15a942563dcb4e68
k-pcdr <127327771+k-pcdr@users.noreply.github.com>
2023-05-03 10:34:21 -0400

    Added some examples to the comments that I made

--- classroom_activities/ex_7a_files.py
+++ classroom_activities/ex_7a_files.py
@@ -29,5 +29,36 @@ contents = f.read()
 print(contents)
 f.close()
 
-# The 'write' mode ('w') will create a new file if the specified file does not exist, or overwrite the contents of the file if it does exist.
+
+# 5
 # When opening a file in Python, if you don't specify a mode, Python will default to 'read' mode ('r'). 
+# So, this is equivalent to the previous example:
+f = open("something_very_unique_file.txt")
+contents = f.read()
+print(contents)
+f.close()
+
+
+# 6
+# The 'write' mode ('w') will create a new file if the specified file does not exist, or overwrite the contents of the file if it does exist.
+f = open("yay_new_file.txt", "w")
+f.write("Here are some words for you.")
+f.close()
+
+# Now let's read the file
+f = open("yay_new_file.txt")
+contents = f.read()
+print(contents)
+f.close()
+
+# Now write into the file again but with different text
+f = open("yay_new_file.txt", "w")
+f.write("I overwrote those words.")
+f.close()
+
+# Now let's read the file again for proof that the original text was overwritten.
+f = open("yay_new_file.txt")
+contents = f.read()
+print(contents)
+f.close()
+
