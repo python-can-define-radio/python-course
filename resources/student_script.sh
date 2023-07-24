b651e4a8b08b120e3fc5536aadc67e8b069bce77
wolverine-is-the-best <127511752+wolverine-is-the-best@users.noreply.github.com>
2023-07-24 13:00:43 -0400

    Create student_script.sh

---
+++ resources/student_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+
+# set screen blank timeout, lock timeout, and bash terminal timeout
+gsettings set org.gnome.desktop.session idle-delay 900
+gsettings set org.gnome.desktop.screensaver lock-delay 1800
+echo "
+
+## Make TMOUT variable arbitrarily large to avoid terminal auto-closing
+export TMOUT=30000'" >> ~/.bashrc
+
+
+# disable middle click to avoid accidental pasting
+# this works for a single session only (non-persistent).
+xmodmap -e "pointer = 1 25 3 4 5 6 7 8 9 10"
+
+# the next part will disable middle-click on login (persistently).
+## create a script that runs the command that we saw above
+scriptFile=Desktop/disable_middle_click.sh
+echo 'xmodmap -e "pointer = 1 25 3 4 5 6 7 8 9 10"' > ~/$scriptFile
+## Mark the script as executable
+chmod 770 ~/$scriptFile
+## Make autostart directory
+mkdir -p ~/.config/autostart
+## Create a .desktop file which Ubuntu/GNOME auto-loads on login
+echo "[Desktop Entry]
+Type=Application
+Name=run_disable_middle_click_script
+X-GNOME-Autostart-enabled=true
+Exec=$HOME/$scriptFile
+" > ~/.config/autostart/disable_middle_click.desktop
