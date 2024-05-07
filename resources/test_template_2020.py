9512de19da5ae94de2d517da7ede2bcf8731c4d0
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2024-05-07 18:05:37 -0400

    Update test_template_2020.py: Migrate notes from test prep

--- resources/test_template_2020.py
+++ resources/test_template_2020.py
@@ -1,59 +1,116 @@
 ## 1
-"THIS STILL NEEDS TO BE ANSWERED"
+
 
 ## 2
-"THIS STILL NEEDS TO BE ANSWERED"
+# Remember to abbreviate.
+
 
 ## 3
-"THIS STILL NEEDS TO BE ANSWERED"
+# The instructions mention "evenly". Ignore that word.
+
 
 ## 4
-"THIS STILL NEEDS TO BE ANSWERED"
+# Remember to abbreviate.
+
 
 ## 5
-"THIS STILL NEEDS TO BE ANSWERED"
+# Type the corrected version of each line.
+# Indicate whether the original line on the test
+# had an error or not using a comment as shown:
+#   x = input("Hello!")         # line 1: no error 
+#   print("Have a good day.")   # line 2: error
+#   print("Words.")             # line 3: error
+#   print("5 is more than 4.")  # line 3: no error
+
 
 ## 6
-"THIS STILL NEEDS TO BE ANSWERED"
+# Type the corrected version of each line.
+# Indicate whether the original line on the test
+# had an error or not using a comment as in question 5.
+
 
 ## 7
-# print("F: Given Description")
+# - Type the answer only, WITHOUT THE LETTER, in a print statement.
+#  Example question:
+#      What color is close to red?
+#          A. Blue
+#          B. Green
+#          C. Teal
+#          D. Orange
+#  You would type...
+#          print("Orange")
+# print("_____")
+
 
 ## 8
-# print("F: Given Description")
+# print("_____")
+
 
 ## 9
-# print("F: Given Description")
+# print("_____")
+
 
 ## 10
-# print("F: Given Description")
+# print("_____")
+
 
 ## 11
-# print("F: Given Description")
+# print("_____")
+
 
 ## 12
-# print("F: Given Description")
+# print("_____")
+
 
 ## 13
-# print("F: Given Description")
+# print("_____")
+
 
 ## 14
-"THIS STILL NEEDS TO BE ANSWERED"
+# Type the corrected version of each line.
+# Indicate whether the original line on the test
+# had an error or not using a comment as in question 5.
+
 
 ## 15
-"THIS STILL NEEDS TO BE ANSWERED"
+# Type the given code here after receiving the test.
+# ...
+# ...
+# Type your part A answer, uncommented, with blanks filled:
+# print("In the code provided, ____ is the variable. I knew what was the variable because ______")
+# Type your part B answer, uncommented:
+# print("_____")
+
 
 ## 16
-"THIS STILL NEEDS TO BE ANSWERED"
+# Note: you must use the user-provided data inside of a print statement to receive full credit.
+
 
 ## 17
-"THIS STILL NEEDS TO BE ANSWERED"
+# Change all three columns of the table provided to match the calculations listed on the test.
+# Change the content of the 'answer' and 'explanation' columns appropriately so 
+# they match the provided calculations.
+# print("""
+# calculation    answer  explanation
+# 56+33          89      addition of the two numbers 
+# 45-5           40      subtraction of the two numbers
+# 65>34          True    checks if first number is greater than second number
+# """)
+
 
 ## 18
-"THIS STILL NEEDS TO BE ANSWERED"
+# Ignore the part that says "create a new file."
+# Type the given code here after receiving the test.
+#  ...
+#  ...
+# Type your explanation:
+print("This did ________ because _________")
+
 
 ## 19
-"THIS STILL NEEDS TO BE ANSWERED"
+# See note on 18.
+
 
 ## 20
-"THIS STILL NEEDS TO BE ANSWERED"
+# Version A: See test prep document.
+# Version B, C: Follow instructions as given.
