8dff3808fff45b627af51e3b309a060ee6eb665e
TaelorS <taelor.s.ritchie@gmail.com>
2024-10-11 09:17:54 -0400

    Create BlockPy_Extra_Blocks.py

---
+++ resources/misc/BlockPy_Extra_Blocks.py
@@ -0,0 +1,29 @@
+## print
+print(___, ___)
+print(___, ___, ___)
+print(___, ___, ___, ___)
+print(___, ___, ___, ___, ___)
+
+## randomness
+import random
+random.sample(___, ___)
+random.randint(___, ___)
+
+## if...elif...else
+if ___:
+  pass
+elif ___:
+  pass
+else:
+  pass
+
+## if...elif...elif...else
+if ___:
+  pass
+elif ___:
+  pass
+elif ___:
+  pass
+else:
+  pass
+
