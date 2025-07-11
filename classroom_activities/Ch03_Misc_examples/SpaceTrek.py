dd337ce764e018bc6e08ae0bca8d909b66f345b9
Wolverine-itb-pcdr <127511752+wolverine-is-the-best@users.noreply.github.com>
2025-07-11 08:49:05 -0400

    Update SpaceTrek.py

--- classroom_activities/Ch03_Misc_examples/SpaceTrek.py
+++ classroom_activities/Ch03_Misc_examples/SpaceTrek.py
@@ -14,19 +14,17 @@ def exit():
 def status(lightyears, air_scrubbers, gorn, fuel, carbon_dioxide):
     console.clear()
     print(f"Lightyears traveled: {lightyears}")
-    print(f"You have {air_scrubbers} air_scrubbers left in your canteen.")
+    print(f"You have {air_scrubbers} air_scrubbers left.")
     print(f"You have {fuel} fuel.")
     print(f"You have {carbon_dioxide} carbon_dioxide.")
     print(f"The gorn are {abs(gorn)} lightyears behind you.")
     print()
 
 
-def refuel(gorn, fuel):
+def rest(gorn, fuel):
     console.clear()
     fuel = 0
-    refueling = ["Your scanners detect a vein of crystallized zorpathite embedded in an asteroid that allow you to refuel.", "Drifting near a gas giants rings you pass through a vapor-rich field of myronex plasma allowing your fuel cells to recharge.", "You found a cloud of calcified borauntium and are able to collect enough to refuel, it turns out the ore contains microscopic void-beings that willingly give up their essence to provide thrust. It's ethical, mostly. They 'want' to help, probably.", "Your shuttle touches down on a moon streaked with ferrolune dust, a quick harvest restores your fuel reserves. The dusts molecular spin creates a harmonic resonance within your shuttles reactor, essentially 'singing' energy to your shipe"
-    ". It's like jazz.", "You intercept a rogue comet trailing frozen shards of xenthium ore, your shuttles automated refinery kicks in allowing you to refuel.", "Your sensors reveal a subspace pocket brimming with liquid quazaranite, the liquid releases energy by rewinding time in a localized micro-pocket giving your shuttle energy that it 'already used' essentially allowing you to use yesterdays fuel."]
-    print(random.choice(refueling))
+    print("You are fully rested.")
     gained = random.randint(7, 14)
     gorn += gained
     print(f"The gorn have gained {gained} lightyears on you.")
@@ -43,7 +41,7 @@ def ahead_full(lightyears, carbon_dioxide, fuel, gorn):
     gained = random.randint(7, 14)
     gorn += gained - traveled
     print(f"You have traveled {traveled} lightyears.")
-    print(f"You have used {fuel_used} fuel used total is now {fuel}.")
+    print(f"You have gained {fuel_used} fuel total is now {fuel}.")
     print(f"You have gained 1 carbon_dioxide total is now {carbon_dioxide}.")
     print(f"The gorn have traveled {gained} lightyears.")
     return lightyears, carbon_dioxide, fuel, gorn
@@ -58,7 +56,7 @@ def ahead_half(lightyears, carbon_dioxide, fuel, gorn):
     gained = random.randint(7, 14)
     gorn += gained - traveled
     print(f"You have traveled {traveled} lightyears.")
-    print(f"You have used 1 fuel used total is now {fuel}.")
+    print(f"You have gained 1 fuel total is now {fuel}.")
     print(f"You have gained 1 carbon_dioxide total is now {carbon_dioxide}.")
     print(f"The gorn have traveled {gained} lightyears.")
     return lightyears, carbon_dioxide, fuel, gorn
@@ -68,7 +66,6 @@ def scrub(air_scrubbers, carbon_dioxide):
     if air_scrubbers > 0:
         air_scrubbers -= 1
         carbon_dioxide = 0
-        print("The air is breathable again.")
     else:
         print("You have no more air_scrubbers available.")
     return air_scrubbers, carbon_dioxide
@@ -85,9 +82,10 @@ def main():
     console.clear()
     print("[green]Welcome to Space Trek![/]")
     print()
-    print("[blue underline]You have stolen a shuttle to make your way across the great Mobian Nebula.[/]")
-    print("[blue underline]The gorn want their shuttle back and are chasing you down! Survive your[/]")
-    print("[blue underline]space trek and outrun the gorn.[/]")
+    print("[blue underline]You have stolen a shuttle from the aliens on Sadius 9.[/]")
+    print("[blue underline]To escape you need to make your way across the great Mobian Nebula.[/]")
+    print("[blue underline]The Gorn want their shuttle back and are chasing you down! [/]")
+    print("[blue underline]Survive your space trek and outrun the Gorn.[/]")
     print()
 
     while not done:
@@ -105,7 +103,7 @@ Q. Quit.[/]""")
         elif choice.lower() == "e":
             status(lightyears, air_scrubbers, gorn, fuel, carbon_dioxide)
         elif choice.lower() == "d":
-            gorn, fuel = refuel(gorn, fuel)
+            gorn, fuel = rest(gorn, fuel)
         elif choice.lower() == "c":
             lightyears, carbon_dioxide, fuel, gorn = ahead_full(lightyears, carbon_dioxide, fuel, gorn)
         elif choice.lower() == "b":
@@ -120,8 +118,7 @@ Q. Quit.[/]""")
         if fuel > 5 and not done:
             print("Your shuttle is running out of fuel.")
         if fuel >  8 and not done:
-            print("Your shuttle is out of fuel.")
-            done = True
+            print("Your shuttle is dead.")
         if 0 < gorn < 15:
             print("The gorn are getting close.")
         elif gorn >= 0 and not done:
