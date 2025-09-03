bf4aa9876ca252a3efd8412808a04e88f273ce1c
Wolverine-itb-pcdr <127511752+wolverine-is-the-best@users.noreply.github.com>
2025-09-03 12:53:41 -0400

    Update student_config_2025_july.sh

--- resources/student_config_2025_july.sh
+++ resources/student_config_2025_july.sh
@@ -2,14 +2,11 @@ rm ~/*.py
 rm ~/Desktop/*.py
 rm ~/Desktop/*/*.py
 rm ~/Desktop/*/*/*.py
-rm ~/*.pyc
-rm ~/Desktop/*.pyc
-rm ~/Desktop/*/*.pyc
-rm ~/Desktop/*/*/*.pyc
-rmdir ~/__pycache__
-rmdir ~/Desktop/__pycache__
-rmdir ~/Desktop/*/__pycache__
-rmdir ~/Desktop/*/*/__pycache__
+
+rm -r ~/__pycache__
+rm -r ~/Desktop/__pycache__
+rm -r ~/Desktop/*/__pycache__
+rm -r ~/Desktop/*/*/__pycache__
 
 dconf reset -f /org/gnome/shell/extensions/dash-to-dock/
 
