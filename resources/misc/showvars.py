8926b190adfd6ddee0c79c7bba3acf84a812d21e
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2025-09-30 13:40:29 -0400

    Update showvars.py: blacklist jupyterlite vars

--- resources/misc/showvars.py
+++ resources/misc/showvars.py
@@ -3,9 +3,13 @@
 
 _realprint = print  # In case print accidentally gets overwritten later
 def _showvars(lcls: dict):
+    def desirable(x):
+        und = x[0].startswith("_")
+        jupyterblacklist = x[0] in ["In", "Out", "get_ipython", "exit", "quit", "open"]
+        return (not und) and (not jupyterblacklist)
     yellow = "\x1b[33m"
     endcol = "\x1b[0m"
-    without_undesirable = filter(lambda x: not x[0].startswith("_"), lcls.items())
+    without_undesirable = filter(desirable, lcls.items())
     varsdisp = map(lambda x: f"│ {x[0]}: {type(x[1]).__name__} = {repr(x[1])}", without_undesirable)
     _realprint("\n")
     _realprint(f"┌────────────── {yellow}Variables{endcol} ───────────────")
